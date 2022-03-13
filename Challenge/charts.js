f479973331f2f3be8e607a1757b2b0daadcbc7f3
Scheinin <scheir3@medtronic.com>
2022-03-13 15:20:54 -0600

    Challenge 5

--- Challenge/charts.js
+++ Challenge/charts.js
@@ -12,7 +12,6 @@ function init() {
         .text(sample)
         .property("value", sample);
     });
-
     // Use the first sample from the list to build the initial plots
     var firstSample = sampleNames[0];
     buildCharts(firstSample);
@@ -39,7 +38,6 @@ function buildMetadata(sample) {
     var result = resultArray[0];
     // Use d3 to select the panel with id of `#sample-metadata`
     var PANEL = d3.select("#sample-metadata");
-
     // Use `.html("") to clear any existing metadata
     PANEL.html("");
 
@@ -68,8 +66,7 @@ function buildCharts(sample) {
     var otu_ids = firstSample.otu_ids
     var otu_labels = firstSample.otu_labels
     var sample_values = firstSample.sample_values
-    // var washfreq = parseFloat(metadata.wfreq);
-    var frequency = parseFloat(metadata.wfreq);
+    var washfreq = parseFloat(firstSample.wfreq);
     
     var filtered_otu_ids = otu_ids.slice(0.10).reverse();
     var filtered_otu_labels = otu_labels.slice(0,10).reverse();
@@ -119,55 +116,27 @@ function buildCharts(sample) {
     var gaugeData = [
       {
         domain: { x: [0, 1], y: [0, 1] },
-        value: frequency,
-        title: { text: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week"},
+        value: washfreq,
+        title: { text: "Belly Button Washing Frequency" },
         type: "indicator",
         mode: "gauge+number",
         gauge: {
-          axis: { range: [null, 10] },
-          bar: { color: "black"},
           steps: [
-            { range: [0, 2], color: "red" },
-            { range: [2, 4], color: "orange" },
-            { range: [4, 6], color: "yellow" },
-            { range: [6, 8], color: "yellowgreen" },
-            { range: [8, 10], color: "green" }
+            {range: [0,2], color: "red"},
+            {range: [2,4], color: "orange"},
+            {range: [4,6], color: "yellow"},
+            {range: [6,8], color: "limegreen"},
+            {range: [8,10], color: "green"},
           ],
-        }
-      }
-    ]; 
-    var gaugeLayout = {
-      width: 500,
-      height: 425,
-      margin: { t: 0, b: 0 } };
-    // 6. Use Plotly to plot the gauge data and layout.
-    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
-  });
- }
-//     var gaugeData = [
-//       {
-//         domain: { x: [0, 1], y: [0, 1] },
-//         value: washfreq,
-//         title: { text: "Belly Button Washing Frequency" },
-//         type: "indicator",
-//         mode: "gauge+number",
-//         gauge: {
-//           steps: [
-//             {range: [0,2], color: "red"},
-//             {range: [2,4], color: "orange"},
-//             {range: [4,6], color: "yellow"},
-//             {range: [6,8], color: "limegreen"},
-//             {range: [8,10], color: "green"},
-//           ],
-//           axis: {range: [null, 10]},
-//           bar: {color: "black"},
-//         },
-//       }] 
+          axis: {range: [null, 10]},
+          bar: {color: "black"},
+        },
+      }] 
 
-//     var gaugeLayout = [{
-//       width: 600, 
-//       height: 500, 
-//       margin: { t: 0, b: 0 } }];
-//   Plotly.newPlot('gauge', gaugeData, gaugeLayout);
-//   });
-// }
+    var gaugeLayout = [{
+      width: 600, 
+      height: 500, 
+      margin: { t: 0, b: 0 } }];
+  Plotly.newPlot('gauge', gaugeData, gaugeLayout);
+  });
+}
